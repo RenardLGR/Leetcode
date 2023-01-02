25a78e80adfdde61bede21a1b69488ab5afc34bd
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-02 21:58:30 +0100

    first commit

---
+++ 2023/January/main.js
@@ -0,0 +1,45 @@
+const hi = 'HELLO'
+const alphaL = 'abcdefghijklmnopqrstuvwxyz'
+const alphaU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+//==========================================================
+// https://leetcode.com/problems/detect-capital/
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+
+ 
+
+// Example 1:
+
+// Input: word = "USA"
+// Output: true
+// Example 2:
+
+// Input: word = "FlaG"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= word.length <= 100
+// word consists of lowercase and uppercase English letters.
+
+var detectCapitalUse = function(word) {
+    if(word.toUpperCase() === word || word.toLowerCase() === word){ //case is consistant
+        return true
+    }else{
+        return word.slice(1).toLowerCase() === word.slice(1)
+    }
+};
+
+// console.log(detectCapitalUse("USA")); // true
+// console.log(detectCapitalUse("leetcode")); // true
+// console.log(detectCapitalUse("FlaG")); // false
+
+
+//================================================================
