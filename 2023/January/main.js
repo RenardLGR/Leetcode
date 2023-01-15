947c2772de12832d7268fbd5c6344210b432eb50
Olivier Reynard <olivier.reynard17@gmail.com>
2023-01-15 04:29:46 +0100

    daily push

--- 2023/January/main.js
+++ 2023/January/main.js
@@ -321,6 +321,9 @@ var fibRec = function(n){
     if(n===1){
         return 1
     }
+    // if(n<2){
+    //     return n
+    // }
     
     return fibRec(n-2) + fibRec(n-1)
 
@@ -330,6 +333,29 @@ var fibRec = function(n){
 // console.log(fibRec(4)); // 3
 
 //Way slower and way much memory than the non recursive version
+
+var fibRecMemo = function(n){
+    let memo = {}
+
+    return f(n)
+    function f(n) {
+        if(n<2){
+            return n
+        }
+        
+        if(memo[n]){
+            return memo[n]
+        }else{
+            memo[n] = f(n-2) + f(n-1)
+            return memo[n]
+        }
+    }
+}
+
+// console.log(fibRecMemo(2)); // 1
+// console.log(fibRecMemo(4)); // 3
+
+//not bad memory and time wise
 
 //================================================
 // https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/description/
@@ -455,4 +481,92 @@ function minimumRoundsBis(tasks) {
 // console.log(minimumRoundsBis([2,2,3,3,2,4,4,4,4,4])); // 4
 
 
-//=================================================
+//==================================================
+// https://leetcode.com/problems/add-two-numbers/
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+// Example 1:
+// 2 -> 4 -> 3
+// 5 -> 6 -> 4
+// ____________
+// 7 -> 0 -> 8
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+ 
+
+// Constraints:
+
+// The number of nodes in each linked list is in the range [1, 100].
+// 0 <= Node.val <= 9
+// It is guaranteed that the list represents a number that does not have leading zeros.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let res = {}
+
+    let val1
+    let val2
+    let carry = 0
+    let next = res
+
+    let isDone = false
+
+    while(!isDone){ //while either of the lists are not finished
+        isDone = true
+        if(l1.next || l2.next){ //if one of the lists has a next element
+            isDone = false
+            let node = {} //create empty node we will dig into
+
+            val1 = l1.val===undefined ? 0 : l1.val
+            val2 = l2.val===undefined ? 0 : l2.val
+            next.val = (val1 + val2 + carry) % 10
+            carry = Math.floor((val1 + val2 + carry) / 10)
+
+            next.next = node //append empty node
+            next = next.next //dig res as we build
+
+            l1 = l1.next || {val : 0} //dig input
+            l2 = l2.next || {val : 0} //dig input
+        }else{ //end of both suits
+            val1 = l1.val===undefined ? 0 : l1.val
+            val2 = l2.val===undefined ? 0 : l2.val
+            next.val = (val1 + val2 + carry) % 10
+            carry = Math.floor((val1 + val2 + carry) / 10)
+
+            if(carry > 0){ //if there is still a carry
+                next.next = {val: 1, next: null}
+            }else{
+                next.next = null
+            }
+        }
+    }
+
+    return res
+};
+
+//=======================================================
