1f284c4a7436efd8410526887fa1e0205b3a1afe
Olivier Reynard <olivier.reynard17@gmail.com>
2023-02-02 11:00:16 +0100

    daily push

--- 2023/February/main.js
+++ 2023/February/main.js
@@ -263,3 +263,53 @@ var findMedianSortedArraysBis = function(nums1, nums2) {
 // console.log(findMedianSortedArraysBis([1,2] , [3,4])); // 2.5
 
 //==================================================
+// https://leetcode.com/problems/palindrome-number/description/
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+ 
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+var isPalindrome = function(x) {
+    if(x < 0){
+        return false
+    }else{
+        let rev = 0
+        let cpy = x
+        while(cpy > 0){
+            rev = rev * 10
+            rev += cpy%10
+            cpy = Math.floor(cpy/10)
+        }
+        return rev === x
+    }
+}
+
+// console.log(isPalindrome(121));
+// console.log(isPalindrome(122));
+
+//=========================================================
