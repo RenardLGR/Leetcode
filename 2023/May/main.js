9919bed032c6fb9ed352cd58c0721fd771e59aed
Olivier Reynard <olivier.reynard17@gmail.com>
2023-05-09 10:37:48 +0200

    daily push

--- 2023/May/main.js
+++ 2023/May/main.js
@@ -24,7 +24,9 @@ function reverseList(head){
 }
 
 let nodeListHead1 = {val:1, next:{val:2, next:{val:3, next:{val:4, next:{val:5, next:null}}}}}
-console.log(reverseList(nodeListHead1))
+// console.log(reverseList(nodeListHead1))
+
+//=======================================================
 // https://leetcode.com/problems/reverse-nodes-in-k-group/
 // Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 
@@ -48,5 +50,52 @@ console.log(reverseList(nodeListHead1))
 // Follow-up: Can you solve the problem in O(1) extra memory space?
 
 var reverseKGroup = function(head, k) {
-    
-};
+    // First check if we can make a reverse group, if so build it and append it. If not, append the remaining nodes
+    let curr = head
+    let temp = null
+    let subGroup = null
+    // let res = new ListNode(null)
+    let res = {val:null, next:null}
+    let resHead = res
+    let isGroupReversed = true
+
+    while(curr){
+        let pointer = curr //pointer will go k nodes ahead to check if we can reverse this group
+        //Check if I add a reverse group or the remaining nodes
+        for(let i=0 ; i<k ; i++){
+            if(!pointer){
+                isGroupReversed = false
+                break
+            }
+            pointer = pointer.next
+        }
+        if(isGroupReversed){
+            //Build the revse sub group
+            for(let i=0 ; i<k ; i++){
+                temp = curr.next
+                curr.next = subGroup
+                subGroup = curr
+                curr = temp
+            }
+            //append it
+            res.next = subGroup
+            subGroup = null
+            //move forward so the next append is at the right position
+            while(res.next){
+                res = res.next
+            }
+        }else{
+            //append the remaining nodes
+            res.next = curr
+            break
+        }
+    }
+
+    return resHead.next
+}
+
+nodeListHead1 = {val:1, next:{val:2, next:{val:3, next:{val:4, next:{val:5, next:null}}}}}
+// console.log(reverseKGroup(nodeListHead1, 5)) // [5, 4, 3, 2, 1]
+// console.log(reverseKGroup(nodeListHead1, 2)) // [2, 1, 4, 3, 5]
+// console.log(reverseKGroup(nodeListHead1, 3)) // [3, 2, 1, 4, 5]
+
