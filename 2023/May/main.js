4ecdbf8bc11a8f660e9f0d65b6e515dc6b177287
Olivier Reynard <olivier.reynard17@gmail.com>
2023-05-12 12:45:47 +0200

    daily push

--- 2023/May/main.js
+++ 2023/May/main.js
@@ -254,3 +254,69 @@ function removeDuplicatesBis(nums){
 // console.log(removeDuplicatesBis([1, 1, 1, 1, 1])); // 1 nums = [1]
 
 //=========================================
+// https://leetcode.com/problems/remove-element/
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int val = ...; // Value to remove
+// int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+// int k = removeElement(nums, val); // Calls your implementation
+
+// assert k == expectedNums.length;
+// sort(nums, 0, k); // Sort the first k elements of nums
+// for (int i = 0; i < actualLength; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+ 
+
+// Example 1:
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Example 2:
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+// Note that the five elements can be returned in any order.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 100
+// 0 <= nums[i] <= 50
+// 0 <= val <= 100
+
+var removeElement = function(nums, val) {
+    //Two pointer:
+    //one is i and run through the array one by one
+    //one is toReplace and replace nums[toReplace] with nums[i] only if nums[i] is different than val, filtering out val and keeping the order (even if it is not necessary)
+    let toReplace = 0
+    for(let i=0 ; i<nums.length ; i++){
+        if(nums[i] !== val){
+            nums[toReplace] = nums[i]
+            toReplace++
+        }
+    }
+
+    // console.log(nums)
+    return toReplace
+}
+
+// console.log(removeElement([3,2,2,3], 3)) // 2, nums = [2,2,_,_]
+// console.log(removeElement([0,1,2,2,3,0,4,2], 2)) // 5, nums = [0, 1, 3, 0, 4, _, _, _]
+console.log(removeElement([0,1,2,3,4,5], 6)) // 6, nums = [0,1,2,3,4,5]
