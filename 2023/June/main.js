51b895ce33f0791c55d4bcc7d9a6481405b246e9
Olivier Reynard <olivier.reynard17@gmail.com>
2023-06-09 03:23:16 +0200

    daily push

--- 2023/June/main.js
+++ 2023/June/main.js
@@ -225,3 +225,93 @@ function longestValidParenthesesTer(s){
 // console.log(longestValidParenthesesTer("(()(()")); // 2
 
 //=============================================================
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
+// Example 3:
+// Input: nums = [1], target = 0
+// Output: -1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 5000
+// -104 <= nums[i] <= 104
+// All values of nums are unique.
+// nums is an ascending array that is possibly rotated.
+// -104 <= target <= 104
+
+function binarySearch(sortedArr, target) {
+    let min = 0
+    let max = sortedArr.length - 1
+    while (max - min >= 0) {
+        let middle = Math.floor((min + max) / 2)
+        if (sortedArr[middle] === target) {
+            return middle
+        } else {
+            if (sortedArr[middle] < target) {
+                min = middle + 1
+            } else {
+                max = middle - 1
+            }
+        }
+    }
+    return -1
+}
+// console.log(binarySearch([1, 2, 3, 5, 7, 8, 9], 3));
+// console.log(binarySearch([1, 2, 3, 5, 7, 8, 9], 6));
+
+
+function searchRotatedSorted(nums, target){
+    //We'll cut nums in half, naturally, one half will be sorted and the other not
+    //At some point both are sorted when we cut right at the start of the pivot
+    //We'll find where of the two halves the target is and keep cutting the remaining half
+    let left = 0
+    let right = nums.length-1
+    while(right - left >= 0){
+        let middle = Math.floor((right + left)/2)
+        if(nums[middle] === target){
+            return middle
+        }
+
+        if(nums[left] <= nums[middle] && target <= nums[middle] && target >= nums[left]){
+            //case left half is sorted and target is in its range
+            right = middle - 1
+        }else if(nums[middle] <= nums[right] && target >= nums[middle] && target <= nums[right]){
+            //case right half is sorted and target is in its range
+            left = middle + 1
+        }else if(nums[left] <= nums[middle] && !(target <= nums[middle] && target >= nums[left])){
+            //case left half is sorted and target is NOT in its range
+            left = middle + 1
+        }else if(nums[middle] <= nums[right] && !(target >= nums[middle] && target <= nums[right])){
+            //case right half is sorted and target is NOT in its range
+            right = middle - 1
+        }
+    }
+
+    return -1
+}
+
+// console.log(searchRotatedSorted([4,5,6,7,0,1,2], 0)); //4
+// console.log(searchRotatedSorted([4,5,6,7,0,1,2], 7)); //3
+// console.log(searchRotatedSorted([4,5,6,7,0,1,2], 3)); //-1
+// console.log(searchRotatedSorted([1], 0)); //-1
+// console.log(searchRotatedSorted([1], 1)); //0
+// console.log(searchRotatedSorted([1, 3], 2)); //-1
+// console.log(searchRotatedSorted([4,5,6,7,8,0,1,2], 3)) // -1
+
+//===========================================
