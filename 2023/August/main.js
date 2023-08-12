ce7dc4c7bc69cbf0b3baaadb242430fb439d1136
Olivier Reynard <olivier.reynard17@gmail.com>
2023-08-12 22:06:18 +0200

    daily push

--- 2023/August/main.js
+++ 2023/August/main.js
@@ -223,3 +223,85 @@ function multiplyBis(a, b){
 }
 
 // console.log(multiplyBis('123', '12')); // "1476"
+
+//==========================================
+// https://leetcode.com/problems/wildcard-matching/
+// Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+
+// '?' Matches any single character.
+// '*' Matches any sequence of characters (including the empty sequence).
+// The matching should cover the entire input string (not partial).
+
+
+// Example 1:
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+
+// Example 2:
+// Input: s = "aa", p = "*"
+// Output: true
+// Explanation: '*' matches any sequence.
+
+// Example 3:
+// Input: s = "cb", p = "?a"
+// Output: false
+// Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+ 
+
+// Constraints:
+// 0 <= s.length, p.length <= 2000
+// s contains only lowercase English letters.
+// p contains only lowercase English letters, '?' or '*'.
+
+// CtrlC/CtrlV from https://leetcode.com/problems/wildcard-matching/solutions/1228581/backtracking-97-faster-javascript-version-of-official-solution-es6/
+
+// Approach: Backtracking
+// Algorithm:
+
+// Initiate 3 pointers:
+// pointer for pattern-index
+// pointer for string-index
+// pointer for temporary-index when we encounter "*" in pattern
+// Initiate a variable for storing index-position of star "*"
+// both string pointer and pattern pointer are moved forward by 1 step whenever:
+// chars in string and pattern match OR
+// char in string matches with "?" in pattern
+// Only pattern pointer is moved by a step when we encounter a "*"
+// Only string pointer is moved when a char doesn't match with char in pattern
+// The starIndex acts as a reference point during entire iteration
+// The temp-index along with string-index are constantly updated when chars in pattern and string do not match.
+// Code:
+
+const isMatch = function (string, pattern) {
+  let s = 0, p = 0;
+  let starIdx = -1, pointer = -1;
+
+  while (s < string.length) {
+    if ((p < pattern.length && string[s] === pattern[p]) || pattern[p] === "?") {
+      s++;
+      p++;
+    } 
+	else if (p < pattern.length && pattern[p] === "*") {
+      starIdx = p;
+      pointer = s;
+      p++;
+    } 
+	else if (starIdx === -1) return false;
+    else {
+      p = starIdx + 1;
+      s = pointer + 1;
+      pointer = s;
+    }
+  }
+  for (let idx = p; idx < pattern.length; idx++) {
+    if (pattern[idx] !== "*") return false;
+  }
+  return true;
+}
+
+// console.log(isMatch("aa", "a")) // false
+// console.log(isMatch("aa", "*")) // true
+// console.log(isMatch("cb", "?a")) // false
+// console.log(isMatch("cb", "?b")) // true
+
