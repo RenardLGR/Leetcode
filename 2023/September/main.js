0856edf5c6ae1272a7bb6e793805a70c10072d32
Olivier Reynard <olivier.reynard17@gmail.com>
2023-09-27 11:13:02 +0200

    daily push

--- 2023/September/main.js
+++ 2023/September/main.js
@@ -429,3 +429,70 @@ function myPowTer(x,n){
 // console.log(myPowTer(0.00001, 2147483647)) // 0 ??
 
 //=====================================
+// https://leetcode.com/problems/n-queens/
+// The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+// Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+
+// Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+
+// Example 1:
+// Input: n = 4
+// Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+// Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+
+// Example 2:
+// Input: n = 1
+// Output: [["Q"]]
+
+// Constraints:
+// 1 <= n <= 9
+
+var solveNQueens = function(n) {
+    let res = []
+    let board = Array.from({length:n}, (_ => Array(n).fill(".")))
+    solve(0)
+    return res.map(matrix => matrix.map(row => row.join('')))
+
+    function solve(row){
+        if(row === n){
+            res.push(deepCpy(board))
+            return
+        }
+
+        for(let col=0 ; col<n ; col++){
+            if(isValid(row, col)){
+                board[row][col] = "Q"
+                solve(row+1)
+                //Backtracking
+                board[row][col] = "."
+            }
+        }
+    }
+
+    function deepCpy(board){
+        return board.map(row => [...row])
+    }
+
+    function isValid(row, col) {
+        // Check if there is a queen in the same column
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === "Q") return false
+        }
+        // Check upper left diagonal
+        for (let i = row, j = col ; i >= 0 && j >= 0 ; i--, j--) {
+            if (board[i][j] === "Q") return false
+        }
+        // Check upper right diagonal
+        for (let i = row, j = col ; i >= 0 && j < n ; i--, j++) {
+            if (board[i][j] === "Q") return false
+        }
+
+        return true
+    }
+}
+
+// console.log(solveNQueens(4)); // [[ '.Q..', '...Q', 'Q...', '..Q.' ], [ '..Q.', 'Q...', '...Q', '.Q..' ]]
+// console.log(solveNQueens(1)); // [ [ 'Q' ] ]
+// console.log(solveNQueens(8), solveNQueens(8).length); // 92 elements
