2f9692a5e6a301384629ce36ef42b5b6f69797e1
Olivier Reynard <olivier.reynard17@gmail.com>
2023-12-09 04:32:38 +0100

    daily push

--- 2023/December/main.js
+++ 2023/December/main.js
@@ -301,3 +301,59 @@ function rotateRightTer(head, k){
 // Good runtime
 
 //=============================================
+// https://leetcode.com/problems/unique-paths/
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Example 1:
+// Input: m = 3, n = 7
+// Output: 28
+
+// Example 2:
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+
+// Constraints:
+// 1 <= m, n <= 100
+
+// Naive : Try every path, count them
+var uniquePaths = function(m, n) {
+    let res = 0
+    solve([0,0])
+    return res
+
+    function solve(pos){
+        const [row, col] = pos
+
+        if(row===m-1 && col===n-1){
+            res++
+            return
+        }
+
+        if(row < m-1){
+            solve([row+1, col])
+        }
+        if(col < n-1){
+            solve([row, col+1])
+        }
+    }
+}
+
+// console.log(uniquePaths(3, 7)) // 28
+// console.log(uniquePaths(3, 2)) // 3
+// console.log(uniquePaths(2, 2)) // 2
+// console.log(uniquePaths(1, 2)) // 1
+// console.log(uniquePaths(1, 1)) // 1
+
+// Too long
+
+function uniquePathsBis(m, n){
+
+}
